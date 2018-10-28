842020357e61a994a1bafac20030fd6176bc8a78
JoelGamer2 <joelgamer2@outlook.com>
2018-10-28 16:39:40 +0100

    texturas rutas

--- src/main/java/RubyCraft/Maquinas/Renderizado/Render_Mesa_de_Ruby.java
+++ src/main/java/RubyCraft/Maquinas/Renderizado/Render_Mesa_de_Ruby.java
@@ -22,7 +22,7 @@ public class Render_Mesa_de_Ruby extends  TileEntitySpecialRenderer {
 	
 	private ResourceLocation textura = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Ruby.png");
 	private ResourceLocation texturaNavidad = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Ruby_Navidad.png");
-	
+	private ResourceLocation texturaHallowen = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Ruby_Hallowen.png");
 	 public Render_Mesa_de_Ruby() {
 		 model = new Mesa_De_Ruby_Render_Model();
 		 
@@ -37,10 +37,12 @@ public class Render_Mesa_de_Ruby extends  TileEntitySpecialRenderer {
 		GL11.glPushMatrix();
 		GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
 		GL11.glRotated(180, 0, 0, 1);
-		if(!RubyCraft.Navidad){
+		if(!RubyCraft.Navidad && !RubyCraft.HalloWen){
 		this.bindTexture(textura);
-		}else if(RubyCraft.Navidad){
+		}else if(RubyCraft.Navidad && !RubyCraft.HalloWen){
 			this.bindTexture(texturaNavidad);
+		}else if(RubyCraft.HalloWen && !RubyCraft.Navidad) {
+			this.bindTexture(texturaHallowen);
 		}
 		this.model.render((Entity)null, 0, -0.1f, 0, 0, 0, 0.0625f);
 		GL11.glPopMatrix();
--- src/main/java/RubyCraft/Maquinas/Renderizado/Render_Mesa_de_Zafiro.java
+++ src/main/java/RubyCraft/Maquinas/Renderizado/Render_Mesa_de_Zafiro.java
@@ -14,7 +14,8 @@ public class Render_Mesa_de_Zafiro extends  TileEntitySpecialRenderer {
 	
 	private ResourceLocation textura = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Zafiro.png");
 	private ResourceLocation texturaNavidad = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Zafiro_Navidad.png");
-	
+	private ResourceLocation texturaHallowen = new ResourceLocation(RubyCraft.modid + ":textures/blocks/TileEntitys/Mesas/Mesa_De_Zafiro_Hallowen.png");
+
 	 public Render_Mesa_de_Zafiro() {
 		 model = new Mesa_De_Zafiro_Render_Model();
 		 	
@@ -27,11 +28,13 @@ public class Render_Mesa_de_Zafiro extends  TileEntitySpecialRenderer {
 		GL11.glPushMatrix();
 		GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
 		GL11.glRotated(180, 0, 0, 1);
-		if(!RubyCraft.Navidad){
-		this.bindTexture(textura);
-		}else if(RubyCraft.Navidad){
-			this.bindTexture(texturaNavidad);
-		}
+		if(!RubyCraft.Navidad && !RubyCraft.HalloWen){
+			this.bindTexture(textura);
+			}else if(RubyCraft.Navidad && !RubyCraft.HalloWen){
+				this.bindTexture(texturaNavidad);
+			}else if(RubyCraft.HalloWen && !RubyCraft.Navidad) {
+				this.bindTexture(texturaHallowen);
+			}
 		this.model.render((Entity)null, 0, -0.1f, 0, 0, 0, 0.0625f);
 		GL11.glPopMatrix();
 		
