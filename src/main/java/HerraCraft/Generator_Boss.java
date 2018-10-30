94594bd1d7c3ded3146f61a6bee4e44825e3dc61
JoelGamer2 <joelgamer2@outlook.com>
2018-10-30 23:08:46 +0100

    comit

--- src/main/java/HerraCraft/Generator_Boss.java
+++ src/main/java/HerraCraft/Generator_Boss.java
@@ -2,6 +2,10 @@ package HerraCraft;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
 public class Generator_Boss extends Block {
 
@@ -10,4 +14,19 @@ public class Generator_Boss extends Block {
 		
 	}
 
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z,EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_) {
+		
+		if(world.isRemote) {
+		
+		 Entity mob = EntityList.createEntityByName("Parca", world);
+         mob.setPosition(x, y + 1, z); 
+		 world.spawnEntityInWorld(mob);
+		}
+		return true;
+			
+		
+	}
+	
 }
