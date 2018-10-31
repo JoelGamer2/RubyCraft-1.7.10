68a0e1444e7b56e0e2d873f23a1241c32ae8d506
Joelgamer2 <joelgamer2@outlook.com>
2018-10-31 23:02:44 +0100

    hola

--- src/main/java/RubyCraft/Control_de_Version.java
+++ src/main/java/RubyCraft/Control_de_Version.java
@@ -5,7 +5,7 @@ public class Control_de_Version {
  public static boolean Version_de_desarrolador = false;
  public static boolean Activar_Version_trol_facil = false;
  public static boolean Navidad_Activar = false;
- 
+ //prueba
 	public static void Iniciar_todo() {
 		
 		if(Version_de_desarrolador) {
