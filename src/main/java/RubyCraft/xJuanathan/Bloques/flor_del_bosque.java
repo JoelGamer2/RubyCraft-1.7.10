6f807dc6f78dba5f7754348d931092e857e856dd
Juanathan <delaverga69@hotmail.com>
2018-11-25 04:50:01 +0100

    Añadir bloques e items

---
+++ src/main/java/RubyCraft/xJuanathan/Bloques/flor_del_bosque.java
@@ -0,0 +1,13 @@
+package RubyCraft.xJuanathan.Bloques;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class flor_del_bosque extends Block{
+
+	public flor_del_bosque(Material p_i45394_1_) {
+		super(p_i45394_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/RubyCraft/xJuanathan/Items/silex.java
@@ -0,0 +1,7 @@
+package RubyCraft.xJuanathan.Items;
+
+import net.minecraft.item.Item;
+
+public class silex extends Item{
+
+}
