c027b8257635ad35e457e74342ef5f496f1205f3
Joelgamer2 <joelgamer2@outlook.com>
2018-12-07 22:43:27 +0100

    borrar

--- src/main/java/RubyCraft/Bases/Encantamientos.java
+++ src/main/java/RubyCraft/Bases/Encantamientos.java
@@ -5,11 +5,29 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.world.World;
 
 public class Encantamientos {
 
+	
+	public static void Borrar_Encantamientos(Enchantment enchantment, ItemStack itemStack) {
+		NBTTagList enchantmentTagList = itemStack.getEnchantmentTagList();
+		if (enchantmentTagList != null) { // If the ItemStack has an enchantment tag list
+			for (int i = 0; i < enchantmentTagList.tagCount(); i++) { // Iterate through the list
+				NBTTagCompound enchantmentTag = enchantmentTagList.getCompoundTagAt(i); // Get the individual enchantment tag
+				if (enchantmentTag.getShort("id") == enchantment.effectId) { // If the enchantment tag is the Enchantment to remove,
+					enchantmentTagList.removeTag(i); // Remove it
+					break;
+				}
+			}
+
+			if (enchantmentTagList.tagCount() == 0) { // If there are no more enchantments on the ItemStack,
+				itemStack.getTagCompound().removeTag("ench"); // Remove the enchantment tag list
+			}
+		}
+	}
 	
 	public static void encantar(ItemStack stack, EntityPlayer player, World world, Item espada, Enchantment encantamiento, Item vial) {
 		if(player.inventory.hasItem(vial) && !world.isRemote) {
@@ -19,7 +37,7 @@ public class Encantamientos {
 			player.inventory.consumeInventoryItem(espada);
 			player.inventory.consumeInventoryItem(vial);
 			if(!(j == 0)) {
-				BorrarEncantamiento.removeEnchantment(encantamiento, stack);
+				Borrar_Encantamientos(encantamiento, stack);
 			}
 			stack.addEnchantment(encantamiento, j + 1);
 			player.inventory.addItemStackToInventory(stack);
