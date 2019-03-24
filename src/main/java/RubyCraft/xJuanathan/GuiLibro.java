7b9a7a3b4a52cf54546ae42075000118f481fe56
Usuario <Usuario@DESKTOP-4N5LGAK>
2019-03-24 02:12:20 +0100

    Gui del libro sin errores.

--- src/main/java/RubyCraft/xJuanathan/GuiLibro.java
+++ src/main/java/RubyCraft/xJuanathan/GuiLibro.java
@@ -13,9 +13,7 @@ import net.minecraft.world.World;
 public class GuiLibro extends GuiContainer{
 	
 
-    public static int ancho;
-    public static int alto;
-	public static int X = 256;
+    public static int X = 256;
 	public static  int Y = 255;
 	private static boolean activo = false;
 	public static int paginas = 0;
@@ -26,69 +24,91 @@ public class GuiLibro extends GuiContainer{
 
 
 	
-	public GuiLibro(InventoryPlayer p_i1084_1_, World p_i1084_2_, int p_i1084_3_, int p_i1084_4_, int p_i1084_5_)
+	public GuiLibro(InventoryPlayer inv, World world, int x, int y, int z)
     {
-        super(new ContainerLibro(p_i1084_1_, p_i1084_2_, p_i1084_3_, p_i1084_4_, p_i1084_5_));
-        
+        super(new ContainerLibro(inv, world, x, y, z));
     }
+	
+	
 	public void updateScreen ()
 	{
-        
         super.updateScreen();
-        
-		
-	}
-	@Override
-	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
-		int centroX = (this.width/2);
-		int centroY = this.height / 2;
-		
-		
-		      GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		      if(paginas==0) {
-	        this.mc.getTextureManager().bindTexture(Textura);
-		      }else if(paginas==1){
-		    	  this.mc.getTextureManager().bindTexture(Textura2);
-		      }
-	        int k = /**(this.width - this.xSize) / 2**/ (this.width/2)-(X/2);
-	        int l = (this.height - this.ySize) / 2;
-	        this.drawTexturedModalRect(k, l, 0, 0, X, Y);
-
-	        if(!activo) {
-				
-						
-	        	this.buttonList.add(this.siguiente = new GuiButton(0, centroX - 30, centroY + 80, "siguiente"));
-				this.buttonList.add(this.atras = new GuiButton(1, centroX + 30, centroY + 80, "atras"));
-				activo = true;
-			}
-	       String paginase = Integer.toString(paginas);
-			this.fontRendererObj.drawString(StatCollector.translateToLocal(paginase), (this.width/2) + 118, (this.height/2) + 63,  0x37FF00);
-
 	}
 	
+	
 	@Override
-	protected void actionPerformed(GuiButton b) {
-		if(b.enabled){
-		if(b.id == 0) {
-       paginas ++;
+	protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) 
+	{
+		int centroX = this.width/2;
+		int centroY = this.height/2;
+		
+		
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		
+		if(paginas==0) 
+		{
+	        this.mc.getTextureManager().bindTexture(Textura);
+		}
+		
+		else if(paginas==1)
+		{
+			this.mc.getTextureManager().bindTexture(Textura2);
+		}
+		
+	    int k = (this.width/2)-(X/2);
+	    int l = (this.height - this.ySize) / 2;
+	    
+	    this.drawTexturedModalRect(k, l, 0, 0, X, Y);
+
+	    String paginase = Integer.toString(paginas);
 	       
+		this.fontRendererObj.drawString(StatCollector.translateToLocal(paginase), (this.width/2) + 118, (this.height/2) + 63,  0x000000);
+	}
+	
+	
+	
+	@Override
+	public void initGui() 
+	{
+		super.initGui();
+		
+		int centroX = this.width/2;
+		int centroY = this.height/2;
+		
+		this.buttonList.add(this.siguiente = new GuiButton(0, centroX - 30, centroY + 80, 20, 10, "siguiente")); //Parametros de los botones (id, x, y, width, height, text)
+		this.buttonList.add(this.atras = new GuiButton(1, centroX + 30, centroY + 80, 20, 10, "atras"));
+	}
+	
+	
+	
+	@Override
+	protected void actionPerformed(GuiButton b) 
+	{
+		if(b.enabled)
+		{
+			if(b.id == 0) 
+			{
+				paginas ++;
 	        }
 		
-		if(b.id == 1) {
-			if(!(paginas == 0)) {
-			paginas--;
+			if(b.id == 1) 
+			{
+				if(!(paginas == 0)) 
+				{
+					paginas --;
+				}
 			}
 		}
-		}
+		
 		super.actionPerformed(b);
 	}
 	    
+	
 	@Override
-	public void onGuiClosed() {
+	public void onGuiClosed() 
+	{
 		activo = false;
 		paginas = 0;
 		super.onGuiClosed();
 	}
-	
-
 }
